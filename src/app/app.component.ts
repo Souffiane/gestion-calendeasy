b6552c40da1e1b4c93c8e90e1e427ee48a517d16
souffiane <souffiane.chouaib@gmail.com>
2019-07-15 23:44:52 +0200

    Depalcement du menu dans le composant root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestion-calendeasy';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  deconnexion() {
+    this.authService.signOut();
+    this.router.navigate(['/login']);
+  }
 }
